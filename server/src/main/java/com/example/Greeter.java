88444e020f1b478f2b3d15b3ddaedc2290c02d24
Sagor Chowdhuri <Sagorchowdhuri@gmail.com>
2018-07-06 16:49:57 +0530

    4nd commit

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,8 +14,8 @@ public class Greeter {
 
   /**
 
-  @param someone
   @return Greeting String
+  @param someone that should be known
   **/
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
